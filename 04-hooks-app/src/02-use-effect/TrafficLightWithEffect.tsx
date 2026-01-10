7d09fc47c91bf18132a3cc7dcc2636db1d6029c5
PatricioOsorio <patricio.osorio@elektra.com.mx>
2026-01-10 15:47:38 -0600

    feat: (seccion 9 - profundizando hooks y react)  Recomendaciones del useEffect

--- 04-hooks-app/src/02-use-effect/TrafficLightWithEffect.tsx
+++ 04-hooks-app/src/02-use-effect/TrafficLightWithEffect.tsx
@@ -9,9 +9,10 @@ const colors: Record<Colors, string> = {
   green: 'bg-green-500 animate-pulse',
 };
 
+const INITIAL_COUNT = 5;
 export const TrafficLightWithEffect = () => {
   const [light, setLight] = useState<Colors>('red');
-  const [countDown, setCountDown] = useState(5);
+  const [countDown, setCountDown] = useState(INITIAL_COUNT);
 
   const getTrafficLightClass = (color: Colors) => {
     return cn('w-32 h-32 rounded-full', light === color ? colors[color] : 'bg-gray-800');
@@ -19,7 +20,7 @@ export const TrafficLightWithEffect = () => {
 
   useEffect(() => {
     if (countDown === 0) return;
-    
+
     const id = setInterval(() => {
       setCountDown((c) => c - 1);
     }, 1000);
@@ -27,11 +28,38 @@ export const TrafficLightWithEffect = () => {
     return () => clearInterval(id);
   }, [countDown]);
 
+  useEffect(() => {
+    if (countDown > 0) return;
+    
+    if (light === 'red') {
+      setLight('green');
+      setCountDown(5);
+      return;
+    }
+    if (light === 'green') {
+      setLight('yellow');
+      setCountDown(2);
+      return;
+    }
+    if (light === 'yellow') {
+      setLight('red');
+      setCountDown(5);
+      return;
+    }
+  }, [countDown, light]);
+
   return (
     <section className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <section className="flex flex-col items-center space-y-8">
         <h1 className="text-center- text-white text-2xl">Traffict light with effect</h1>
         <h2 className="text-center- text-white text-xl">{countDown}</h2>
+
+        <div className="w-64 bg-green-50 rounded-full h-2">
+          <div
+            className="bg-green-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+            style={{ width: `${(countDown / INITIAL_COUNT) * 100}%` }}
+          ></div>
+        </div>
 
         <div className={getTrafficLightClass('red')}></div>
         <div className={getTrafficLightClass('yellow')}></div>
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+// import { TrafficLight } from './01-use-state/TrafficLight';
 import { TrafficLightWithEffect } from './02-use-effect/TrafficLightWithEffect';
 
 createRoot(document.getElementById('root')!).render(
