34dde4ae90f192664039021151cd656c7409a62d
Patricio Miguel Osorio Osorio <patriciomiguel_12@hotmail.com>
2026-02-12 16:44:16 -0600

    feat: (Sección 12 - Use Context) React Router - Contexto de usuario - Información global

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,7 +1,43 @@
-import { type PropsWithChildren } from 'react';
+import { createContext, useMemo, useState, type PropsWithChildren } from 'react';
+import type { IUser } from '../data/use-mock.data';
 
-export type IUserContextProps = PropsWithChildren;
+export type TAuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 
-export const UserContextProvider = ({ children }: IUserContextProps) => {
-  return <div>{children}</div>;
+export interface IUserContextProps {
+  authStatus: TAuthStatus;
+  user: IUser | null;
+  onLogin: (id: number) => boolean;
+  onLogout: () => void;
+}
+
+// ! Context
+export const UserContext = createContext({} as IUserContextProps);
+
+// ! HOC
+export type IUSerContextProviders = PropsWithChildren;
+
+export const UserContextProvider = ({ children }: IUSerContextProviders) => {
+  const [authStatus, setAuthStatus] = useState<TAuthStatus>('checking');
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const handleLogin = (id: number) => {
+    console.log(`login id:${id}`);
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log('logout');
+  };
+
+  const value = useMemo(
+    () => ({
+      authStatus,
+      user,
+      onLogin: handleLogin,
+      onLogout: handleLogout,
+    }),
+    []
+  );
+
+  return <UserContext value={value}>{children}</UserContext>;
 };
