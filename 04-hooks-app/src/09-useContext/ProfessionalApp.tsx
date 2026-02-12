fe53038a4e3385451b78746e546bf5f0e2bc5ae9
Patricio Miguel Osorio Osorio <patriciomiguel_12@hotmail.com>
2026-02-12 12:35:14 -0600

    feat: (Sección 12 - Use Context) React Router - Manejo de rutas

--- 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
+++ 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
@@ -1,6 +1,10 @@
+import { RouterProvider } from 'react-router';
+import { appRouter } from './router/app.router';
+
 export const ProfessionalApp = () => {
   return (
     <article className="page">
+      <RouterProvider router={appRouter} />
       <h1>Professional App</h1>
     </article>
   );
