9907e6f9181019858538e5b6e92e246a4da94f21
Patricio Miguel Osorio Osorio <patriciomiguel_12@hotmail.com>
2026-02-13 17:23:06 -0600

    feat: (Sección 13 - Single Page Application - SPA) Parte 2 - Pensemos en componentes

--- 05-heroes-app/src/heroes/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/search/SearchPage.tsx
@@ -1,7 +1,15 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '../components/HeroStats';
+
 const SearchPage = () => {
   return (
     <article className="page">
-      <h1>SearchPage</h1>
+      <CustomJumbotron
+        title="Search heroes"
+        subtitle="Discover, and administer heroes and villains "
+      />
+
+      <HeroStats />
     </article>
   );
 };
