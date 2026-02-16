55d351c920cc466d2106490b2ffe9375c4db531d
Patricio Miguel Osorio Osorio <patriciomiguel_12@hotmail.com>
2026-02-16 12:12:17 -0600

    feat: (Sección 13 - Single Page Application - SPA) Menú superior - Identificar ruta activa

---
+++ 05-heroes-app/src/components/custom/CustomMenu.tsx
@@ -0,0 +1,41 @@
+import { Link, useLocation } from 'react-router';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '../ui/navigation-menu';
+import { cn } from '@/lib/utils';
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => pathname === path;
+
+  const classNameActive = (path: string) => isActive(path) && 'bg-slate-600';
+
+  const elements = [
+    { to: '/', label: 'Home' },
+    { to: '/search', label: 'Search heroes' },
+  ];
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {elements.map((item) => (
+          <NavigationMenuItem key={item.to}>
+            <NavigationMenuLink
+              asChild
+              className={cn(navigationMenuTriggerStyle(), classNameActive(item.to))}
+            >
+              <Link to={item.to} viewTransition>
+                {item.label}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
--- 05-heroes-app/src/heroes/layouts/HeroesLayout.tsx
+++ 05-heroes-app/src/heroes/layouts/HeroesLayout.tsx
@@ -1,8 +1,10 @@
+import { CustomMenu } from '@/components/custom/CustomMenu';
 import { Outlet } from 'react-router';
 
 export const HeroesLayout = () => {
   return (
     <article className="page">
+      <CustomMenu />
       <Outlet />
     </article>
   );
