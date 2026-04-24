3865738ffe882fd8972b64285d2a85a031f51637
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-04-23 21:58:09 -0600

    feat(06-teslo-shop) - eslint config

--- 06-teslo-shop/eslint.config.ts
+++ 06-teslo-shop/eslint.config.ts
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactPlugin from '@eslint-react/eslint-plugin'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -12,6 +13,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactPlugin.configs['recommended-typescript'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
